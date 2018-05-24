13bfce300c977430fb238a7e1e979eff82ce84d7
Andre Lobato <andrefelipelobato@gmail.com>
2018-05-25 10:46:05 +1200

    discover_config

--- flower/utils/actions.py
+++ flower/utils/actions.py
@@ -1,11 +1,11 @@
 import os
 
-from scheduler.core import discover_actions
-from scheduler.settings import LOGDIR
+from scheduler.core import discover_configs
+from scheduler.settings import LOGDIR, ACTIONS_DIR
 import tailer
 
 def get_action_conf(action_id):
-    actions = discover_actions()
+    actions = discover_configs(ACTIONS_DIR)
     if actions.has_key(action_id):
         with open(actions[action_id]) as conf:
             action_conf = conf.read()
