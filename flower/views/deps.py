c2e419ea2362c982cdd2877a68ddd56bea20e22a
Diego Miranda <dsousamiranda@gmail.com>
2023-08-16 16:53:54 +1200

    add events coroutine

--- flower/views/deps.py
+++ flower/views/deps.py
@@ -7,15 +7,22 @@ import ast
 import os
 import shelve
 import datetime
+import threading
+import types
 
 try:
     from itertools import imap
 except ImportError:
     imap = map
 
+from functools import wraps
+from datetime import timedelta
 import tornado
+import tornado.ioloop
 from tornado import web
+from tornado import gen
 from tornado.concurrent import run_on_executor
+from concurrent.futures import ThreadPoolExecutor
 
 from ..views import BaseHandler
 from ..utils.tasks import get_task_by_id
@@ -38,13 +45,54 @@ def resolve_deps_for_cycle(action_id, cycle_dt,):
     else:
         workflow = beat.cycles_workflows[cycle_dt]
         return resolve_deps(action_id, workflow, upstream=True)
+
+io_loop = tornado.ioloop.IOLoop.current()
+
+THREADPOOL_MAX_WORKERS = 10
+THREADPOOL_TIMEOUT_SECS = 30
+
+def onthread(function):
+    @gen.coroutine
+    def decorated(self, *args, **kwargs):
+        future = executed(self, *args, **kwargs)
+        try:
+            response = yield gen.with_timeout(
+                timedelta(seconds=THREADPOOL_TIMEOUT_SECS), future)
+            if isinstance(response, types.GeneratorType):  # subthreads
+                response = yield gen.with_timeout(
+                    timedelta(seconds=THREADPOOL_TIMEOUT_SECS),
+                    next(response))
+        except gen.TimeoutError as exc:
+            future.cancel()
+            raise exc
+        self.write(response)
+
+    @run_on_executor
+    def executed(*args, **kwargs):
+        return function(*args, **kwargs)
+
+    return decorated
+    
+def blocking(method):
+    """
+    Wraps the method in an async method, and executes the function on `self.executor`
+    """
+
+    @wraps(method)
+    async def wrapper(self, *args, **kwargs):
+        def work():
+            return method(self, *args, **kwargs)
+        return await io_loop.run_in_executor(self.executor, work)
+
+    return wrapper
     
 class DependencyPydotView(BaseHandler):
     def __init__(self, *args, **kwargs):
         super(DependencyPydotView, self).__init__(*args,**kwargs)
-        self.pool = self.application.pool
+        self.executor = ThreadPoolExecutor(max_workers=4)
 
-    @run_on_executor(executor='pool')
+    # @run_on_executor
+    @onthread
     def _plot_deps(self, task_id):
         task = get_task_by_id(self.application.events, task_id)
         if task:
