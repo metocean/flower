e62f87edf463b13df55d8ca5fef4639ca81a961e
Diego Miranda <dsousamiranda@gmail.com>
2024-05-06 12:10:05 +1200

    add docstring

--- flower/utils/search.py
+++ flower/utils/search.py
@@ -4,6 +4,21 @@ from kombu.utils.encoding import safe_str
 
 
 def parse_search_terms(raw_search_value):
+    """
+    Parses the raw search value and returns a dictionary containing the parsed search terms.
+
+    Args:
+        raw_search_value (str): The raw search value to be parsed.
+
+    Returns:
+        dict: A dictionary containing the parsed search terms. The keys in the dictionary
+              represent different search categories, such as 'result', 'args', 'kwargs', and 'state'.
+              The values associated with each key are the corresponding parsed search values.
+
+    Example:
+        >>> parse_search_terms('result:success args:1 kwargs=foo=bar state:running')
+        {'result': 'success', 'args': ['1'], 'kwargs': {'foo': 'bar'}, 'state': ['running']}
+    """
     search_regexp = r'(?:[^\s,"]|"(?:\\.|[^"])*")+'  # splits by space, ignores space in quotes
     if not raw_search_value:
         return {}
@@ -35,13 +50,27 @@ def parse_search_terms(raw_search_value):
 
 
 def satisfies_search_terms(task, search_terms):
+    """
+    Check if a task satisfies the given search terms.
+
+    Args:
+        task (Task): The task to be checked.
+        search_terms (dict): A dictionary containing the search terms.
+
+    Returns:
+        bool: True if the task satisfies the search terms, False otherwise.
+    """
     any_value_search_term = search_terms.get('any')
     result_search_term = search_terms.get('result')
     args_search_terms = search_terms.get('args')
     kwargs_search_terms = search_terms.get('kwargs')
     state_search_terms = search_terms.get('state')
-    
-    if not any([any_value_search_term, result_search_term, args_search_terms, kwargs_search_terms, state_search_terms]):
+
+    if not any([any_value_search_term,
+                result_search_term,
+                args_search_terms, 
+                kwargs_search_terms,
+                state_search_terms]):
         return True
     
     terms = [
@@ -81,10 +110,29 @@ def stringified_dict_contains_value(key, value, str_dict):
 
 
 def preprocess_search_value(raw_value):
+    """
+    Preprocesses the search value by removing leading and trailing double quotes and spaces.
+
+    Args:
+        raw_value (str): The raw search value.
+
+    Returns:
+        str: The preprocessed search value.
+    """
     return raw_value.strip('" ') if raw_value else ''
 
 
 def task_args_contains_search_args(task_args, search_args):
+    """
+    Check if the given task arguments contain all the search arguments.
+
+    Args:
+        task_args (list): The task arguments to search in.
+        search_args (list): The search arguments to check for.
+
+    Returns:
+        bool: True if all search arguments are present in the task arguments, False otherwise.
+    """
     if not task_args:
         return False
     return all(a in task_args for a in search_args)
